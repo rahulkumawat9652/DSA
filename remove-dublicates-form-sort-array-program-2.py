368353f8e2ea052f2252448c3a6fc6bd1d0526e3
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-08-20 18:40:50 +0530

    remove dublicates form sort array program 2

---
+++ remove-dublicates-form-sort-array-program-2.py
@@ -0,0 +1,10 @@
+nums = [1,1,2,3,4,4,7,9,9,9,10]
+n = len(nums)
+freq_map = {}
+for i in range(0,n):
+    freq_map[nums[i]] = 0
+j = 0
+for k in freq_map:
+    nums[j] = k
+    j+=1
+print(freq_map)
