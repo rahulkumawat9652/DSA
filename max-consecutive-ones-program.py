e67a38b3118fa58ed66fb7cb54e93f6a3ebe4cab
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-08-24 21:51:58 +0530

    max consecutive ones program

---
+++ max-consecutive-ones-program.py
@@ -0,0 +1,12 @@
+nums = [1,1,0,1,1,1,1,1,0,1,1,1]
+n = len(nums)
+count = 0
+max_count = 0
+for i in range(0,len(nums)):
+    if nums[i]==1:
+        count+=1
+    else:
+        max_count = max(max_count,count)
+        count = 0
+print(count)
+print(max(max_count,count))
