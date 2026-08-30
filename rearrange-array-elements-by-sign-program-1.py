a4fc4d59cac59e4045cab5b4846af1626199373c
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-08-30 10:48:37 +0530

    rearrange array element by sign program 1

---
+++ rearrange-array-elements-by-sign-program-1.py
@@ -0,0 +1,12 @@
+nums = [5,10,-3,-1,-10,6]
+pos = []
+neg = []
+for i in range(0,len(nums)):
+    if nums[i]>=0:
+        pos.append(nums[i])
+    else:
+        neg.append(nums[i])
+for i in range(0,len(pos)):
+    nums[2*i] = pos[i]
+    nums[(2*i)+1] = neg[i]
+print(nums)
