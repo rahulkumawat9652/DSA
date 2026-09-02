2be93f7a9825b0b2de32a08ca7f01ea6596161fb
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-09-02 20:19:01 +0530

    print lower triangle matrix program

---
+++ print-lower-triangle-matrix-program.py
@@ -0,0 +1,10 @@
+nums = [[1,2,3],[4,5,6],[7,8,9]]
+rows = len(nums)
+cols = len(nums[0])
+for i in range(0,rows):
+    for j in range(0,cols):
+        if j<=i:
+            print(nums[i][j],end=" ")
+        else:
+            print(" ",end=" ")
+    print()
