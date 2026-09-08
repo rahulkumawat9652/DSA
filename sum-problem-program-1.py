6b8be0be2391973bc06086fd6907d1cb03c55c8d
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-09-08 20:47:26 +0530

    sum problem program 1

---
+++ sum-problem-program-1.py
@@ -0,0 +1,11 @@
+arr = [-1,0,1,2,2,-1,-4]
+n = len(arr)
+my_set = set()
+for i in range(0,n):
+    for j in range(i+1,n):
+        for k in range(j+1,n):
+            if arr[i]+arr[j]+arr[k] == 0:
+                temp = [arr[i],arr[j],arr[k]]
+                temp.sort()
+                my_set.add(tuple(temp))
+print([list(ans) for ans in my_set])
