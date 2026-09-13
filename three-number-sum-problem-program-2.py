400e68b95129da3436d95c784a445c6fe6854784
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-09-13 21:35:03 +0530

    three number sum problem program 2

---
+++ three-number-sum-problem-program-2.py
@@ -0,0 +1,13 @@
+arr = [-1,0,2,1,-1,-4]
+n = len(arr)
+result = set()
+for i in range(0,n):
+    my_set = set()
+    for j in range(i+1,n):
+        third = -(arr[i]+arr[j])
+        if third in my_set:
+            temp = [arr[i],arr[j],third]
+            temp.sort()
+            result.add(tuple(temp))
+        my_set.add(arr[j])
+print([list(ans) for ans in result]) 
