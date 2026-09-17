6cc68a51e5d5ed8596e14aaab3418e5f04dde98e
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-09-17 22:03:51 +0530

    implrmrntstion of  lower bound program

---
+++ implrmrntstion-of -lower-bound-program.py
@@ -0,0 +1,14 @@
+nums = [1,1,1,2,2,2,4,4,5,5,6,6,6,6,7,8,10]
+n = len(nums)
+lb = n
+target = 6
+low = 0
+high= n-1
+while low <= high:
+    mid = (low+high)//2
+    if nums[mid] >= target:
+        lb = mid
+        high = mid-1
+    else:
+        low = mid +1
+print(lb)
