e36b9a807e905643cb3c91a2d1bd4561655fcf52
Rahul kumawat <rahulkumawat9652@gmail.com>
2026-09-24 15:51:12 +0530

    bubble sort method 4

---
+++ bubble-sort-method4.py
@@ -0,0 +1,11 @@
+num = [5, 7, 8, 4, 1, 6, 9, 2]
+n = len(num)
+for i in range(0, n-1):
+    swap = False
+    for j in range(i, n-1):
+        if num[j] > num[j+1]:
+            num[j], num[j+1] = num[j+1], num[j]
+            swap = True
+    if swap == False:
+        break
+print(num)
